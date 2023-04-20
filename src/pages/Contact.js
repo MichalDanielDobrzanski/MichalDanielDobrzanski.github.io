7e243303ccda5d8d6b7bcb6e27d98d4ce9fd9725
michaldobrzanski <dobrzanski.michal.daniel@gmail.com>
2023-04-20 13:38:18 +0200

    added react pages

---
+++ src/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Contact() {
+  return (
+      <Container>
+        <h1>Welcome to My Landing Page</h1>
+        <p>
+          This is a simple landing page created with React and Bootstrap.
+        </p>
+      </Container>
+  );
+}
+
+export default Contact;
+
